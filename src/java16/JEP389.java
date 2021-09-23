b952d845d19cb4b58fea54111d2ee4a55a4ea815
Woltring, I.A.H. (Ivo) <Ivo.Woltring@devolksbank.nl>
2021-09-23 07:23:02 +0200

    updated foreing in java 16

--- src/java16/JEP389.java
+++ src/java16/JEP389.java
@@ -1,23 +1,19 @@
-package java16;
 
 import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.FunctionDescriptor;
 import jdk.incubator.foreign.LibraryLookup;
 
-import java.io.File;
 import java.lang.invoke.MethodType;
 import java.nio.file.Path;
 
-// navigate to the src folder
-// make sure you are using jdk 16
-// javac --add-modules jdk.incubator.foreign java16/JEP389.java
+// ./jstart.sh 16
+// javac --add-modules jdk.incubator.foreign JEP389.java
 // java --add-modules jdk.incubator.foreign -Dforeign.restricted=permit java16.JEP389
 
 public class JEP389 {
   public static void main(final String[] args) throws Throwable {
-    final String fileResource = getFileResource("java16/helloworld.so");
     var lib = LibraryLookup
-          .ofPath(Path.of(fileResource)); //fully qualified path
+          .ofPath(Path.of("/src/helloworld.so")); //fully qualified path
 
     var sym = lib.lookup("helloworld").get();
     var fd = FunctionDescriptor.ofVoid();
@@ -25,18 +21,6 @@ public class JEP389 {
     var mh = CLinker.getInstance()
                     .downcallHandle(sym.address(), mt, fd);
     mh.invokeExact();
-  }
-
-  /**
-   * Get a filname from the recourse folder.
-   *
-   * @param fileName the filename to get in src/test/resources
-   * @return the absolute path to the filename
-   */
-  public static String getFileResource(final String fileName) {
-    String abspath = new File(".").getAbsolutePath();
-    abspath = abspath.substring(0, abspath.length() - 1);
-    return new File(abspath + fileName).getAbsolutePath();
   }
 
 }
--- src/java16/helloworld.c
+++ src/java16/helloworld.c
@@ -4,6 +4,13 @@
 //gcc -c -fpic helloworld.c
 //gcc -shared -o helloworld.so helloworld.o
 
+
+//If you use my "jstart.sh 16" command you will get a clean docker image
+//apt-get update
+//apt-get install -y gcc
+//gcc -c -fpic helloworld.c
+//gcc -shared -o helloworld.so helloworld.o
+
 void helloworld() {
-	printf("Hello Java 17 lovers!\n");
+	printf("Hello Java 16 lovers!\n");
 }
