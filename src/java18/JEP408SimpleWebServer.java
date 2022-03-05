7a73f5d8c6602ca86da9c3b38c1d30e9e45c7887
Ivo Woltring <i.woltring@anva.nl>
2022-03-05 23:05:16 +0100

    Java 18 - JEP 413

--- src/java18/JEP408SimpleWebServer.java
+++ src/java18/JEP408SimpleWebServer.java
@@ -8,14 +8,6 @@ import static com.sun.net.httpserver.SimpleFileServer.OutputLevel;
 /**
  * A Simple Static WebServer
  * <br/>
- * Snippet you write yourself:
- *
- * {@snippet :
- *  var server = com.sun.net.httpserver.SimpleFileServer.createFileServer(new InetSocketAddress(8000),
- *               Path.of("/src"), com.sun.net.httpserver.SimpleFileServer.OutputLevel.VERBOSE);
- *  server.start();
- * }
- * <br/>
  * Snippet referenced in the code:
  * {@snippet class="JEP408SimpleWebServer" region="example"}
  */
@@ -28,13 +20,10 @@ public class JEP408SimpleWebServer {
               Path.of("/src"), // @replace regex="/src" replacement="PATH_HERE"
               OutputLevel.VERBOSE);  // @link regex="OutputLevel" target="SimpleFileServer.OutputLevel"
         server.start();
-        System.out.println( "Started: http://localhost:8000" );
         // @end
+        System.out.println( "Started: http://localhost:8000" );
     }
 }
 
 
-//To get this example to work for the snippit javadoc
-//$ javadoc --snippet-path . -d doc JEP408SimpleWebServer.java
-//$ jwebserver -b 0.0.0.0 -p 8000
-//open http://localhost:8000/doc
+
--- src/java18/JEP413Snippet.md
+++ src/java18/JEP413Snippet.md
@@ -1,3 +1,15 @@
 # JEP 413: Code Snippets in Java API Documentation
 
 See code of `JEP408SimpleWebServer`
+
+- To get this example to work for the snippet javadoc
+
+```shell
+$ javadoc --snippet-path . -d doc JEP408SimpleWebServer.java
+$ jwebserver -b 0.0.0.0 -p 8000
+```
+
+- open http://localhost:8000/doc
+
+
+![](JEP413.png)
